c203c9921ada884d90885cda76c1641d4f43ae5f
SuperAuMan <makejack4@gmail.com>
2019-04-20 01:53:26 +0700

    next-routes

--- server.js
+++ server.js
@@ -1,32 +1,33 @@
-const express = require("express");
-const next = require("next");
-const server = express();
-const bodyParser = require("body-parser");
+const next = require('next')
+const routes = require('./routes')
+const express = require('express')
+const server = express()
+const bodyParser = require('body-parser')
 
-const dev = process.env.NODE_ENV !== "production";
-const app = next({ dev });
-const handle = app.getRequestHandler();
+const dev = process.env.NODE_ENV !== 'production'
+const app = next({ dev })
+const handle = routes.getRequestHandler(app)
 
-server.use(bodyParser.urlencoded({ extended: true }));
-server.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }))
+server.use(bodyParser.json())
 
 //require
-require("dotenv").config();
+require('dotenv').config()
 
 app
   .prepare()
   .then(() => {
     // require("./routes/GeneralRoute")(app);
-    server.get("*", (req, res) => {
-      return handle(req, res);
-    });
+    server.get('*', (req, res) => {
+      return handle(req, res)
+    })
 
-    server.listen(3000, err => {
-      if (err) throw err;
-      console.log("> Ready on http://localhost:3000");
-    });
+    server.use(handle).listen(3000, err => {
+      if (err) throw err
+      console.log('> Ready on http://localhost:3000')
+    })
   })
   .catch(ex => {
-    console.error(ex.stack);
-    process.exit(1);
-  });
+    console.error(ex.stack)
+    process.exit(1)
+  })
