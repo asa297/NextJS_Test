ad9af91f1ac1d1d59a0994762c888fd647f79ce8
SuperAuMan <makejack4@gmail.com>
2019-04-29 23:37:36 +0700

    react-virtualized List

--- components/Shared/ListComponents/ListTable.js
+++ components/Shared/ListComponents/ListTable.js
@@ -1,42 +1,52 @@
 import React from 'react'
-import ReactTable from 'react-table'
+import styled from 'styled-components'
+import { List, AutoSizer } from 'react-virtualized'
 
 class virtualized extends React.PureComponent {
+  state = {
+    data: [],
+  }
+
+  componentWillMount() {
+    for (let i = 0; i < 1000; i++) {
+      let { data } = this.state
+      const result = { id: i, username: `username${i}` }
+      data.push(result)
+      this.setState({ data })
+    }
+  }
+  renderRow({ index, key, style }) {
+    const { data } = this.state
+    return (
+      <div key={key} style={style}>
+        {data[index].username}
+      </div>
+    )
+  }
+
   render() {
-    const data = [
-      {
-        name: 'Tanner Linsley',
-        age: 26,
-        friend: {
-          name: 'Jason Maurer',
-          age: 23,
-        },
-      },
-    ]
+    const { data } = this.state
 
-    const columns = [
-      {
-        Header: 'Name',
-        accessor: 'name', // String-based value accessors!
-      },
-      {
-        Header: 'Age',
-        accessor: 'age',
-        Cell: props => <span className="number">{props.value}</span>, // Custom cell components!
-      },
-      {
-        id: 'friendName', // Required because our accessor is not a string
-        Header: 'Friend Name',
-        accessor: d => d.friend.name, // Custom value accessors!
-      },
-      {
-        Header: props => <span>Friend Age</span>, // Custom header components!
-        accessor: 'friend.age',
-      },
-    ]
-
-    return <ReactTable data={data} columns={columns} />
+    return (
+      <Container>
+        <AutoSizer>
+          {({ width, height }) => (
+            <List width={width} height={height} rowHeight={50} rowRenderer={rowRenderer => this.renderRow(rowRenderer)} rowCount={data.length} />
+          )}
+        </AutoSizer>
+      </Container>
+    )
   }
 }
 
 export default virtualized
+
+const Container = styled.div`
+  @media (min-width: 600px) {
+    height: calc(100vh - 64px);
+  }
+  height: calc(100vh - 56px);
+`
+const ListView = styled.div`
+  cursor: pointer;
+`
--- pages/_document.js
+++ pages/_document.js
@@ -18,9 +18,9 @@ export default class MyDocument extends Document {
           <meta name="viewport" content="initial-scale=1.0, width=device-width" />
           <link rel="icon" type="image/x-icon" href="/static/logo/shopping.ico" />
           <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
-          <link rel="stylesheet" type="text/css" href="/static/react-table.css" />
+          {/* <link rel="stylesheet" type="text/css" href="/static/react-virtualzed.css" /> */}
         </Head>
-        <body style={{ margin: '0px' }}>
+        <body style={{ margin: '0px', overflowX: 'hidden' }}>
           <Main />
           <NextScript />
         </body>
