bbe2635e0fe0c3a654cee928d3b1dda623a94d2b
SuperAuMan <makejack4@gmail.com>
2019-05-01 00:41:07 +0700

    fix layout ListVirtualized

--- pages/org.js
+++ pages/org.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { connect } from 'react-redux'
+import styled from 'styled-components'
 import { admin } from '<helpers>/role'
 import { getPageNameFromReq } from '<helpers>/utils'
 import { withAuth, ModalLoading, ButtonNew, ListVirtualized, ListRender } from '<components>'
@@ -27,7 +28,10 @@ class index extends React.PureComponent {
     } = this.props
     return (
       <>
-        <ListVirtualized rowRenderer={row => ListRender({ ...row, data })} rowCount={data.length} rowHeight={50} />
+        <Container>
+          <ListVirtualized rowRenderer={row => ListRender({ ...row, data })} rowCount={data.length} rowHeight={50} />
+        </Container>
+
         <ButtonNew hide={!isList} onClick={() => alert('test')} />
         <ModalLoading loading={isFetching} text={'Loading...'} />
       </>
@@ -41,3 +45,9 @@ index = connect(
 )(index)
 
 export default withAuth([admin])(index)
+
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  padding-top: 5px;
+`
