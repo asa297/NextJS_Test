ae89e44e761aa59c1f2aeee41cf0f3b18a41aaac
SuperAuMan <makejack4@gmail.com>
2019-05-05 23:00:10 +0700

    set search input padding

--- components/Shared/ListComponents/SearchBar/index.js
+++ components/Shared/ListComponents/SearchBar/index.js
@@ -1,5 +1,13 @@
 import { Input } from 'antd'
+import styled from 'styled-components'
+const Search = Input.Search
 
 export default ({ ...rest }) => {
-  return <Input.Search {...rest} />
+  return <InputWrapper {...rest} />
 }
+
+const InputWrapper = styled(Search)`
+  > :first-child {
+    padding: 20px 15px;
+  }
+`
