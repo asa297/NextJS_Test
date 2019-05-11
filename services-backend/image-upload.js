480847cc5216a4da4a30c6e766c48fd8794906e0
SuperAuMan <makejack4@gmail.com>
2019-05-11 16:37:28 +0700

    delete set config aws in upload-image

--- services-backend/image-upload.js
+++ services-backend/image-upload.js
@@ -2,12 +2,6 @@ const aws = require('aws-sdk')
 const multer = require('multer')
 const multerS3 = require('multer-s3')
 
-aws.config.update({
-  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-  region: process.env.AWS_RESGION,
-})
-
 const s3 = new aws.S3()
 
 const fileFilter = (req, file, cb) => {
