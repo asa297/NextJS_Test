8f4fca7859ab44684b04ea5a028dcc55793d1d10
SuperAuMan <makejack4@gmail.com>
2019-05-17 22:17:03 +0700

    insert po

--- api/PurchaseOrder/index.js
+++ api/PurchaseOrder/index.js
@@ -1,13 +1,16 @@
 const { ValidateToken, ValidateRole } = require('../../middlewares/AuthMiddleware')
 const { admin, accountant } = require('../../helpers/role')
 const mongoose = require('mongoose')
-const sellerModel = mongoose.model('sellers')
+const orgModel = mongoose.model('organizations')
 const gruopModel = mongoose.model('groups')
+const sellerModel = mongoose.model('sellers')
+
 const itemModel = mongoose.model('items')
+const purchaseOrderModel = mongoose.model('poes')
 
 module.exports = server => {
   server.get('/api/po/groups', ValidateToken, ValidateRole([admin, accountant]), async (req, res) => {
-    const result = await gruopModel.find({}, { groupCode: 1, groupStickerNumber: 1, guideName: 1 })
+    const result = await gruopModel.find({}, { orgId: 1, groupCode: 1, groupStickerNumber: 1, guideName: 1 })
     res.send(result)
   })
 
@@ -23,26 +26,49 @@ module.exports = server => {
   })
 
   server.post('/api/po', ValidateToken, ValidateRole([admin, accountant]), async (req, res) => {
-    // const { orgType, orgName, orgComA, orgComB, orgCode } = req.body
-    // const { user } = req
-    // const found = await organizationModel.findOne({ orgCode })
-    // if (found) return res.status(403).send({ message: 'Organization Code is Duplicate.' })
+    const {
+      group,
+      seller,
+      listItems,
+      subTotal,
+      grandTotalDiscount,
+      discount,
+      credit,
+      grandTotalCreditCharge,
+      creditCharge,
+      receiveCash,
+      changeCash,
+      grandTotal,
+    } = req.body
+    const { user } = req
 
-    // await organizationModel({
-    //   orgTypeId: orgType.id,
-    //   orgTypeName: orgType.label,
-    //   orgName,
-    //   orgComA,
-    //   orgComB,
-    //   orgCode,
-    //   RecordIdBy: user.name,
-    //   RecordNameBy: user.nickname,
-    //   RecordDate: Date.now(),
-    //   LastModifyById: user.name,
-    //   LastModifyByName: user.nickname,
-    //   LastModifyDate: Date.now(),
-    // }).save()
+    const orgQuery = await orgModel.findById(group.orgId)
+    const groupQuery = await gruopModel.findById(group._id)
+    const sellerQuery = await sellerModel.findById(seller._id)
 
-    res.send({ message: 'Organization is already inserted.' })
+    await purchaseOrderModel({
+      orderId: Date.now(),
+      org: orgQuery,
+      group: groupQuery,
+      seller: sellerQuery,
+      listItems,
+      subTotal,
+      discount: grandTotalDiscount,
+      discountPercent: discount,
+      credit,
+      creditCharge: grandTotalCreditCharge,
+      creditchargePercent: creditCharge,
+      receiveCash,
+      changeCash,
+      grandTotal,
+      RecordIdBy: user.name,
+      RecordNameBy: user.nickname,
+      RecordDate: Date.now(),
+      LastModifyById: user.name,
+      LastModifyByName: user.nickname,
+      LastModifyDate: Date.now(),
+    }).save()
+
+    res.send({ message: 'Purchase Order is already inserted.' })
   })
 }
